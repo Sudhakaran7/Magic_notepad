f750a421f0eb0fbf644d1d962f0b40c89a025597
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-07-28 19:30:31 +0530

    Create Magic_Notepad.py

---
+++ Magic_Notepad.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minSteps(self, n):
+        ans = 0
+        d = 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n /= d
+            d += 1
+        return ans
+val=Solution()
+n=int(input())
+print(val.minSteps(n))
